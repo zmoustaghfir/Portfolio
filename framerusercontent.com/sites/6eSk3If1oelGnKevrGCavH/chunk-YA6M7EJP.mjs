2bb075a8b9632eec77a0c5bdb499ecf8bee1d09e
moustaghfir zakaria <113710350+zmoustaghfir@users.noreply.github.com>
2024-02-18 14:51:52 +0100

    Add files via upload

---
+++ framerusercontent.com/sites/6eSk3If1oelGnKevrGCavH/chunk-YA6M7EJP.mjs
@@ -0,0 +1,2 @@
+import{j as a,k as o}from"./chunk-IBNL73MK.mjs";var r="default"in o?a:o,t={},e=r;t.createRoot=e.createRoot;t.hydrateRoot=e.hydrateRoot;var s=t.createRoot,f=t.hydrateRoot;export{s as a,f as b};
+//# sourceMappingURL=chunk-YA6M7EJP.mjs.map
